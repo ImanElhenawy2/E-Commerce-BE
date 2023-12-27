2bb7d2c063475359302cde0cf47fa3af2056f4b9
ImanElhenawy <ziad.omar04@hotmail.com>
2023-12-27 03:16:58 +0100

    url

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use("/brands", brandsRouter);
 
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || "http://ecommerce.onrender.com";
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
